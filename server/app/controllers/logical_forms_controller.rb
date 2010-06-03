a35195cab933a546abcc79e7f51e0cb9ee742bd1
Daniel de Kok <me@danieldk.eu>
2010-06-03 07:54:49 -0700

    PostgreSQL lowercases relation names, use underscores instead.

--- server/app/controllers/logical_forms_controller.rb
+++ server/app/controllers/logical_forms_controller.rb
@@ -2,8 +2,8 @@ class LogicalFormsController < ApplicationController
   def index
     @logical_forms = LogicalForm.find(:all,
     :joins => 'LEFT JOIN judgments ON judgments.logical_form_id = logical_forms.id',
-    :select => 'logical_forms.id AS id, key, COUNT(DISTINCT judgments.user_id) AS judgmentCount',
-    :group => 'logical_forms.id')
+    :select => 'logical_forms.id AS id, key, COUNT(DISTINCT judgments.user_id) AS judgment_count',
+    :group => 'logical_forms.id, logical_forms.key')
     
     respond_to do |format|
       format.xml  { render :xml => @logical_forms }

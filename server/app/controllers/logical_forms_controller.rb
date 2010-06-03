0c8d7aad3f2c97810087d73e4958051768449231
Daniel de Kok <me@danieldk.eu>
2010-06-03 07:37:23 -0700

    Count the number of judgments per logical form.

--- server/app/controllers/logical_forms_controller.rb
+++ server/app/controllers/logical_forms_controller.rb
@@ -1,7 +1,10 @@
 class LogicalFormsController < ApplicationController
   def index
-    @logical_forms = LogicalForm.find(:all, :select => 'id, key')
-
+    @logical_forms = LogicalForm.find(:all,
+    :joins => 'LEFT JOIN judgments ON judgments.logical_form_id = logical_forms.id',
+    :select => 'logical_forms.id AS id, key, COUNT(DISTINCT judgments.user_id) AS judgmentCount',
+    :group => 'logical_forms.id')
+    
     respond_to do |format|
       format.xml  { render :xml => @logical_forms }
       format.json { render :json => @logical_forms }
